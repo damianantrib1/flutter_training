78bf8bfe1ab26ec2079bf51b821073f27a758b5c
damian.turski <damian.turski@icloud.com>
2024-12-17 00:17:01 +0100

    xzxz

---
+++ first_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+runApp(
+    MaterialApp(  
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors:[Colors.deepPurple,Colors.amber,
+              ],
+          begin:Alignment.topLeft,
+          end:Alignment.bottomRight,
+        ),
+          ),
+          child: const Center(
+          child: Text('Hello World !',style: TextStyle(
+            color: Colors.white,fontSize: 30,
+          ))
+        ),
+      ),
+    ),
+  ),
+);
+  }
